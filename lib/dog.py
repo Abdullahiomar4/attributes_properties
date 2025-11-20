2ecd952c80a94fb3edb3e3d4629621e5e0546d03
Abdullahiomar4 <abdullahiomar58@icloud.com>
2025-11-20 10:44:02 +0300

    Fix Dog and Person classes with properties

--- lib/dog.py
+++ lib/dog.py
@@ -1,30 +1,33 @@
 class Dog:
-    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+    approved_breeds = [
+        "Mastiff", "Chihuahua", "Corgi", "Shar Pei", 
+        "Beagle", "French Bulldog", "Pug", "Pointer"
+    ]
 
-    def __init__(self, name="Fido", breed="Pug"):
-        self.name = name
-        self.breed = breed
+    def __init__(self, dog_name="Fido", dog_breed="Pug"):
+        self.dog_name = dog_name
+        self.dog_breed = dog_breed
 
-    # name property
+    # dog_name property
     @property
-    def name(self):
-        return self._name
+    def dog_name(self):
+        return self._dog_name
 
-    @name.setter
-    def name(self, value):
-        if isinstance(value, str) and 1 <= len(value) <= 25:
-            self._name = value
+    @dog_name.setter
+    def dog_name(self, new_value):
+        if isinstance(new_value, str) and 1 <= len(new_value) <= 25:
+            self._dog_name = new_value.title()
         else:
-            print("Name must be string between 1 and 25 characters.")
+            print(f"Invalid name '{new_value}': Name must be a string between 1 and 25 characters.")
 
-    # breed property
+    # dog_breed property
     @property
-    def breed(self):
-        return self._breed
+    def dog_breed(self):
+        return self._dog_breed
 
-    @breed.setter
-    def breed(self, value):
-        if value in self.approved_breeds:
-            self._breed = value
+    @dog_breed.setter
+    def dog_breed(self, new_value):
+        if new_value in self.approved_breeds:
+            self._dog_breed = new_value
         else:
-            print("Breed must be in list of approved breeds.")
+            print(f"Invalid breed '{new_value}': Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -1,31 +1,33 @@
 class Person:
-    approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance",
-                     "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
+    approved_jobs = [
+        "Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance",
+        "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"
+    ]
 
-    def __init__(self, name="John Doe", job="Sales"):
-        self.name = name
-        self.job = job
+    def __init__(self, person_name="John Doe", person_job="Sales"):
+        self.person_name = person_name
+        self.person_job = person_job
 
-    # name property
+    # person_name property
     @property
-    def name(self):
-        return self._name
+    def person_name(self):
+        return self._person_name
 
-    @name.setter
-    def name(self, value):
-        if isinstance(value, str) and 1 <= len(value) <= 25:
-            self._name = value.title()
+    @person_name.setter
+    def person_name(self, new_value):
+        if isinstance(new_value, str) and 1 <= len(new_value) <= 25:
+            self._person_name = new_value.title()
         else:
-            print("Name must be string between 1 and 25 characters.")
+            print(f"Invalid name '{new_value}': Name must be a string between 1 and 25 characters.")
 
-    # job property
+    # person_job property
     @property
-    def job(self):
-        return self._job
+    def person_job(self):
+        return self._person_job
 
-    @job.setter
-    def job(self, value):
-        if value in self.approved_jobs:
-            self._job = value
+    @person_job.setter
+    def person_job(self, new_value):
+        if new_value in self.approved_jobs:
+            self._person_job = new_value
         else:
-            print("Job must be in list of approved jobs.")
+            print(f"Invalid job '{new_value}': Job must be in list of approved jobs.")
